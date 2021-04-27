bdb9d947d480fd434590aeb42e284fd3128c9624
Shreyam Adhikari <shreyam1008@gmail.com>
2021-04-27 23:42:40 +0545

    make blog cards

--- src/Components/Tlog.jsx
+++ src/Components/Tlog.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { Card, CardContent, Grid, Typography } from "@material-ui/core";
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
+} from "@material-ui/core";
 
 import { blog } from "../data";
 
 const useStyles = makeStyles((theme) => ({
   root: {
+    display: "flex",
+  },
+  cardGrid: {
     display: "flex",
     flexDirection: "column",
     justifyContent: "center",
@@ -13,7 +22,14 @@ const useStyles = makeStyles((theme) => ({
     padding: 10,
   },
   card: {
-    margin: 20,
+    display: "flex",
+    margin: theme.spacing.unit * 1,
+  },
+  cardContent: { display: "flex", flexDirection: "column" },
+  cardMedia: {
+    justifyContent: "right",
+    width: 30,
+    paddingTop: "56.25%", // 16:9
   },
 }));
 
@@ -21,13 +37,26 @@ const Tlog = () => {
   const classes = useStyles();
   return (
     <div>
-      <Grid className={classes.root}>
+      <Grid container spacing={40} className={classes.cardGrid}>
         {blog.map((post, i) => (
           <Card index={i} className={classes.card}>
-            <CardContent>
+            <CardContent className={classes.cardContent}>
               <Typography variant="headline">{post.title}</Typography>
+              <Typography variant="subheading" color="textSecondary">
+                {post.date}
+              </Typography>
               <Typography variant="subheading">{post.description}</Typography>
+              <Typography variant="subheading" color="primary">
+                Continue reading...
+              </Typography>
             </CardContent>
+            {/* fix image not showing */}
+            <CardMedia
+              component="img"
+              className={classes.cardMedia}
+              image={post.thumbnail}
+              title="something"
+            />
           </Card>
         ))}
       </Grid>
